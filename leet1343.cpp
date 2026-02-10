25fe642a19b4523645143ec2f51c1db7defa53b5
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-10 16:37:33 +0530

    Create leet1343.cpp

---
+++ leet1343.cpp
@@ -0,0 +1,17 @@
+1342. Number of Steps to Reduce a Number to Zero
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int a=0;
+        while(num!=0){
+            if(num%2==0)
+                num=num/2;
+            else
+                num=num-1;
+            a++;
+            
+        }
+        return a;
+    }
+};
