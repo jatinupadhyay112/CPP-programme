435b453121e925567d09f2c0592f4c54413a8482
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-11 16:20:37 +0530

    Create leet2652.cpp

---
+++ leet2652.cpp
@@ -0,0 +1,24 @@
+2652. Sum Multiples
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        for (int i=1;i<=n;i++){
+            if(i%3==0){
+                sum=sum+i;
+                continue;}
+            
+            else if(i%5==0){
+                sum=sum+i;
+                continue;}
+            
+            else if(i%7==0){
+                sum=sum+i;
+                continue;}
+            else
+                continue;
+        }
+        return sum;
+    }
+};
