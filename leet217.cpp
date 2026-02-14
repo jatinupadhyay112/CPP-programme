c016a86eca112ace7ca4af313c60940bbfbb5df2
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-14 19:44:42 +0530

    Create leet217.cpp

---
+++ leet217.cpp
@@ -0,0 +1,21 @@
+217. Contains Duplicate
+
+bruteforce 
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int size=nums.size();
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[i]==nums[j])
+                    return 1;
+                
+            }
+        }
+        return 0;
+    }
+};
+
+optimised 
+
