eb55bc26b023c217cc3484f46883c8b61057764c
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-14 20:05:48 +0530

    Update leet217.cpp

--- leet217.cpp
+++ leet217.cpp
@@ -19,3 +19,16 @@ public:
 
 optimised 
 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        for(int i=0; i < nums.size()-1;i++){
+            if (nums[i]==nums[i+1]){
+                return 1;}
+        }
+        return 0;
+    }
+};
+
