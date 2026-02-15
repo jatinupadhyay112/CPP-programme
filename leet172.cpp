6edda710a03c4f332a24206ea2c34fbff930dbaf
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-15 20:52:55 +0530

    Update leet172.cpp

--- leet172.cpp
+++ leet172.cpp
@@ -21,3 +21,17 @@ public:
         return count;
     }
 };
+
+optimised 
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        while(n){
+            count=count+(n/5);
+            n=n/5;
+        }
+        return count;
+    }
+};
