49358bed8bca2742b48eef50243ab4911515f9c9
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-02-17 14:50:08 +0530

    Create leet977.cpp

---
+++ leet977.cpp
@@ -0,0 +1,14 @@
+977. Squares of a Sorted Array
+
+bruteforce 
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
