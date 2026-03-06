0d54424d8db28774c9c882e8f53fec3b1017f0a3
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-03-06 15:26:49 +0530

    Update leet16.cpp

--- leet16.cpp
+++ leet16.cpp
@@ -21,5 +21,29 @@ public:
     }
 };
 
+
 optimize 
 
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int sum,closum=nums[0]+nums[1]+nums[2];
+        sort (nums.begin(), nums.end()); 
+        for(int i=0;i<nums.size()-2;i++){
+            int j=i+1,k=nums.size()-1;
+            while(j<k){
+                sum=nums[i]+nums[j]+nums[k];
+                if(abs(closum-target)>abs(sum-target))
+                    closum=sum;
+                if(sum<target)
+                    j++;
+                else if(sum>target)
+                    k--;
+                else
+                    return target;
+            }
+    }
+        return closum;
+    }
+};
