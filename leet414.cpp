4357e8537056f1c22cc835f56cb0f981cc5e81e3
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-04-16 12:25:49 +0530

    Create leet414.cpp

---
+++ leet414.cpp
@@ -0,0 +1,20 @@
+414. Third Maximum Number
+
+bruteforce 
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int n=nums.size(),i=n-1,count=1;
+        sort(nums.begin(),nums.end());
+        for (int i =n-1;i>0;i--){
+            if(nums[i]!=nums[i-1]){
+                count++;
+            }
+            if(count ==3)
+                return nums[i-1];
+        }
+        return nums[n-1];
+            
+    }
+};
