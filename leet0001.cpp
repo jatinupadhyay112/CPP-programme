c7f274e0bb32dc8910e55e96019d0ce24ceeb533
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-10 07:14:44 +0000

    prac

--- leet0001.cpp
+++ leet0001.cpp
@@ -19,3 +19,31 @@ public:
 
 //optimised 
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector <pair<int,int>> a;
+        int i=0;
+        while(i<n){
+            a.push_back({nums[i],i});
+            i++;
+        }
+        sort(a.begin(),a.end());
+        i=0;
+        int j=n-1;
+        while(i<j){
+            if(a[i].first+a[j].first==target){
+                return {a[i].second,a[j].second};
+            }
+            else if(a[i].first+a[j].first>target){
+                j--;
+            }
+            else if(a[i].first+a[j].first<target){
+                i++;
+            }
+        }
+
+    return {};
+    }
+};
