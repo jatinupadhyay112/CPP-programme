2d005343e92487eb691aa9bf92ef508a826a7b84
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-12 05:31:17 +0000

    prac

--- leet0053.cpp
+++ leet0053.cpp
@@ -41,3 +41,21 @@ public:
         return maxs;
     }
 };
+
+//Best Optimal 
+
+//By Kaden's Algorithm
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size(),maxs=INT_MIN,sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+            maxs=max(sum,maxs);
+            if(sum<0)
+            sum=0;
+        }
+        return maxs;
+    }
+};
