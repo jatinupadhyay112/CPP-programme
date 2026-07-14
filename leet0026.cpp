41382776db275aad49a8b1d0ef65031ce5901c92
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-14 17:22:40 +0000

    prac

--- leet0026.cpp
+++ leet0026.cpp
@@ -5,26 +5,43 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int count=0;
-        for(int i=0;i<nums.size();i++){
+        int count=0,n=nums.size();
+        for(int i=0;i<n;i++){
             if(nums[i]==100){
                 count=count+1;
                 break;
             }
         }
-        for(int i=0;i<nums.size()-1;i++){
-            for(int j=i+1;j<nums.size();j++){
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
                 if(nums[i]==nums[j])
                     nums[j]=100;
             }
         }
         sort(nums.begin(),nums.end());
         
-        for(int i=0;i<nums.size()&&nums[i]<100;i++)
+        for(int i=0;i<n&&nums[i]<100;i++)
             count++;
         return count;
     }
 };
 
-optimize 
+// better solution
 
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n=nums.size(),a=nums[0],count=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]==a){
+                nums[i]=100;
+            }
+            else{
+            a=nums[i];
+            count++;
+            }
+        }
+        sort(nums.begin(),nums.end());
+        return count;
+    }
+};
