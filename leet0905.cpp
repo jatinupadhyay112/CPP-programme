e364d6e3adfb1354b124aa213a3c57f1c3c4a33a
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-15 18:24:16 +0000

    prac

--- leet0905.cpp
+++ leet0905.cpp
@@ -24,3 +24,28 @@ public:
         return nums;
     }   
 };
+
+//optimal but wrong answer
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int n=nums.size(),i=0,j=n-1;
+        while(i<n&&j>-1){
+            if(nums[i]%2==0){
+                i++;
+                continue;
+            }
+            if(nums[j]%2!=0){
+                j--;
+                continue;
+            }
+            if(nums[i]%2!=0&&nums[j]%2==0){
+                swap(nums[i],nums[j]);
+                i++;
+                j--;
+            }
+        }
+        return nums;
+    }
+};
