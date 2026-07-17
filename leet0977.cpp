7465b738ebc512e411d233ab94945af758811b86
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-17 23:32:35 +0530

    prac

--- leet0977.cpp
+++ leet0977.cpp
@@ -12,3 +12,30 @@ public:
         return nums;
     }
 };
+
+//optimal
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> a(n);
+        int i=0,j=n-1,k=n-1;
+        while(i<=j){
+            if(abs(nums[i])<=abs(nums[j])){
+                a[k]=nums[j]*nums[j];
+                j--;
+                k--;
+            }
+            else{
+                a[k]=nums[i]*nums[i];
+                i++;
+                k--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+      return a;
+    }
+};
