a2c8e69fa4c7abb83c2d4a4271a0cbd17d48c3b3
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-18 19:46:03 +0530

    prac

--- leet0011.cpp
+++ leet0011.cpp
@@ -14,3 +14,20 @@ public:
     }
 };
 
+//optimal
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size(),max_ar=0,ar,i=0,j=n-1;
+        while(i<j){
+            ar=min(height[i],height[j])*(j-i);
+            max_ar=max(ar,max_ar);
+            if(height[i]<height[j])
+            i++;
+            else
+            j--;
+        }
+        return max_ar;
+    }
+};
