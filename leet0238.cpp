4e70e7a356312fb18e32c95034409bdcc53df949
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-20 11:46:17 +0000

    prac

--- leet0238.cpp
+++ leet0238.cpp
@@ -19,3 +19,19 @@ public:
 
 //optimal 
 
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size(),pre=1,suf=1;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(pre);
+            pre=pre*nums[i];
+        }
+        for(int i=n-1;i>-1;i--){
+            ans[i]=ans[i]*suf;
+            suf=suf*nums[i];
+        }
+        return ans;
+    }
+};
