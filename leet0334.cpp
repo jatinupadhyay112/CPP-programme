e3eeace5042db641df169a16ba53c8266a0cb55e
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-21 16:37:31 +0530

    prac

---
+++ leet0334.cpp
@@ -0,0 +1,17 @@
+//bruteforce 
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n-2;i++){
+            for(int j=i+1;j<n-1;j++){
+                for(int k=j+1;k<n;k++){
+                    if(nums[i]<nums[j]&&nums[j]<nums[k])
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+};
