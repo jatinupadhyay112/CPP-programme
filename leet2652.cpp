476d413e414eea7ab8750cabf8710926c89d9531
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-29 23:38:40 +0530

    prac

--- leet2652.cpp
+++ leet2652.cpp
@@ -1,5 +1,7 @@
 //2652. Sum Multiples
 
+//bruteforce but worst
+
 class Solution {
 public:
     int sumOfMultiples(int n) {
@@ -18,6 +20,20 @@ public:
                 continue;}
             else
                 continue;
+        }
+        return sum;
+    }
+};
+
+//better code
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0||i%5==0||i%7==0)
+            sum=sum+i;
         }
         return sum;
     }
