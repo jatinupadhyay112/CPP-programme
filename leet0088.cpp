ab41e9897094d9d6472bb53604fcf7a64aedf238
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-07-29 21:22:53 +0530

    prac

--- leet0088.cpp
+++ leet0088.cpp
@@ -17,5 +17,53 @@ public:
     }
 };
 
-optimal 
+//optimal 
 
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if(m!=0){
+            int j=m-1,k=n-1,i=m+n-1;
+            while(j>=0&&k>=0){
+                if(nums1[j]>nums2[k]){
+                    nums1[i]=nums1[j];
+                    j--;
+                    i--;
+                }
+                else if(nums1[j]<nums2[k]){
+                    nums1[i]=nums2[k];
+                    k--;
+                    i--;
+                }
+                else{
+                    nums1[i]=nums2[k];
+                    k--;
+                    i--;
+                    nums1[i]=nums1[j];
+                    j--;
+                    i--;
+                }
+            }
+            if(k>-1){
+                while(i>-1){
+                    nums1[i]=nums2[k];
+                    k--;
+                    i--;
+                }
+            }
+            else{
+                while(i>-1){
+                    nums1[i]=nums1[j];
+                    j--;
+                    i--;
+                }
+            }
+            
+        }
+        else{
+            for(int i=0;i<n;i++){
+                nums1[i]=nums2[i];
+            }
+        }
+    }
+};
