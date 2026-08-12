b90e3b9a09ec7d78d9b476d1de8043d4391e6c69
jatinupadhyay112 <143349296+jatinupadhyay112@users.noreply.github.com>
2026-08-12 07:18:18 +0000

    prac

---
+++ leet0125.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string sn;
+        for(char c:s){
+            c=tolower(c);
+            if((c>96&&c<123)||(c>47&&c<58))
+            sn.push_back(c);
+        }
+        int i=0,j=sn.size()-1;
+        while(i<j){
+            if(sn[i]==sn[j]){
+                i++;
+                j--;
+            }
+            else
+            return 0;
+        }
+        return 1;
+    }
+};
